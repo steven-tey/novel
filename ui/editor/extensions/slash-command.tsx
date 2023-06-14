0aaf6eed9613d74a1b43adc982f23bdc30822445
Steven Tey <stevensteel97@gmail.com>
2023-06-14 18:10:15 -0500

    refactor to useCompletion

--- ui/editor/extensions/slash-command.tsx
+++ ui/editor/extensions/slash-command.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import React, { useState, useEffect, useCallback, ReactNode } from "react";
 import { Extension } from "@tiptap/core";
 import Suggestion from "@tiptap/suggestion";
@@ -6,6 +8,7 @@ import { useCompletion } from "ai/react";
 import tippy from "tippy.js";
 import { Edit3, Bold, Heading1, Heading2, Italic } from "lucide-react";
 import LoadingCircle from "@/ui/shared/loading-circle";
+import { toast } from "sonner";
 
 const Command = Extension.create({
   name: "slash-command",
@@ -106,10 +109,28 @@ const CommandList = ({
 }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const { complete, completion, isLoading } = useCompletion({
+  const { complete, isLoading } = useCompletion({
     api: "/api/generate",
-    onResponse: () => {
-      editor.chain().focus().deleteRange(range).run();
+    onResponse: async (response) => {
+      if (!response.ok) {
+        toast.error("Failed to generate text.");
+      }
+
+      const data = response.body;
+      if (!data) return;
+
+      editor.chain().focus().deleteRange(range).run(); // remove the slash command
+
+      const reader = data.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        editor.chain().focus().insertContent(chunkValue).run();
+      }
     },
     onFinish: (_prompt, completion) => {
       editor.commands.setTextSelection({
@@ -135,13 +156,8 @@ const CommandList = ({
   );
 
   useEffect(() => {
-    console.log({ completion });
-  }, [completion]);
-
-  useEffect(() => {
     const navigationKeys = ["ArrowUp", "ArrowDown", "Enter"];
     const onKeyDown = (e: KeyboardEvent) => {
-      console.log({ e });
       if (navigationKeys.includes(e.key)) {
         e.preventDefault();
         if (e.key === "ArrowUp") {
