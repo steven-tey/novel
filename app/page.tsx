123b375d308e133da263e8f91aa452b4d4c75bd8
Steven Tey <stevensteel97@gmail.com>
2023-06-15 15:03:46 -0500

    remove incorrect metatags

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import Editor from "@/ui/editor";
 
-export const metadata: Metadata = {
-  title: "Novel",
-  description: "Novel demo",
-};
-
 export default function Page() {
   return (
     <div className="flex min-h-screen flex-col items-center sm:justify-center">
