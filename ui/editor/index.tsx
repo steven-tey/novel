8ddee1cff5b679938d651737932595c9cd27bc80
Steven Tey <stevensteel97@gmail.com>
2023-06-16 09:54:24 -0500

    added autofocus

--- ui/editor/index.tsx
+++ ui/editor/index.tsx
@@ -54,6 +54,7 @@ export default function Editor() {
       setSaveStatus("Unsaved");
       debouncedUpdates(e);
     },
+    autofocus: "end",
   });
 
   // Hydrate the editor with the content from localStorage.
@@ -74,7 +75,12 @@ export default function Editor() {
   }, [isLoading, editor, completion]);
 
   return (
-    <div className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg">
+    <div
+      onClick={() => {
+        editor?.chain().focus().run();
+      }}
+      className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg"
+    >
       <div className="absolute right-5 top-5 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
         {saveStatus}
       </div>
