99ef2230678d2c3c5e827264a5f5e9fceec2fdaf
Steven Tey <stevensteel97@gmail.com>
2023-06-16 09:34:55 -0500

    fix metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,23 @@ import { cal, inter } from "@/styles/fonts";
 import Providers from "./providers";
 import { Metadata } from "next";
 
+const title =
+  "Novel – Notion-style WYSYWIG editor with AI-powered autocompletions";
+const description =
+  "Novel is a Notion-style WYSYWIG editor with AI-powered autocompletions. Built with Tiptap, OpenAI, and Vercel AI SDK.";
+
 export const metadata: Metadata = {
-  title: "Novel – Notion-style WYSYWIG editor with AI-powered autocompletions",
-  description:
-    "Novel is a Notion-style WYSYWIG editor with AI-powered autocompletions. Built with Tiptap, OpenAI, and Vercel AI SDK.",
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+  },
+  twitter: {
+    title,
+    description,
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
