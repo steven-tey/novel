e5d5d2904e54fbe45aa5e9e9af7c8c2828351448
Steven Tey <stevensteel97@gmail.com>
2023-06-15 19:05:03 -0500

    fix paddings

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Editor from "@/ui/editor";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center sm:justify-center">
+    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
       <Editor />
     </div>
   );
