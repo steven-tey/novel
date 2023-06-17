a1b6747a2d1cd47455805cf908bd930c0435c934
Steven Tey <stevensteel97@gmail.com>
2023-06-16 22:49:48 -0500

    edited logos

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Editor from "@/ui/editor";
+import Github from "@/ui/shared/github";
 
 export default function Page() {
   return (
@@ -6,16 +7,23 @@ export default function Page() {
       <a
         href="/deploy"
         target="_blank"
-        className="absolute bottom-5 right-5 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:top-5"
+        className="absolute bottom-5 left-5 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:top-5"
       >
         <svg
-          width={28}
+          width={22}
           viewBox="0 0 76 76"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#000000" />
         </svg>
+      </a>
+      <a
+        href="/github"
+        target="_blank"
+        className="absolute bottom-5 right-5 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:top-5"
+      >
+        <Github />
       </a>
       <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
         <Editor />
