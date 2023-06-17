94b034c3453cf2eb7bdbf99d9082c754e5381f57
Steven Tey <stevensteel97@gmail.com>
2023-06-16 22:19:29 -0500

    edited default text

--- ui/editor/index.tsx
+++ ui/editor/index.tsx
@@ -24,8 +24,57 @@ export default function Editor() {
         content: [
           {
             type: "text",
-            text: "Novel is a Notion-style WYSIWYG editor with AI-powered autocompletions. ",
+            text: "Novel is a Notion-style WYSIWYG editor with AI-powered autocompletions. Built with ",
           },
+          {
+            type: "text",
+            marks: [
+              {
+                type: "link",
+                attrs: {
+                  href: "https://tiptap.dev/",
+                  target: "_blank",
+                  class:
+                    "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+                },
+              },
+            ],
+            text: "Tiptap",
+          },
+          { type: "text", text: ", " },
+          {
+            type: "text",
+            marks: [
+              {
+                type: "link",
+                attrs: {
+                  href: "https://openai.com/",
+                  target: "_blank",
+                  class:
+                    "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+                },
+              },
+            ],
+            text: "OpenAI",
+          },
+          { type: "text", text: ", and the " },
+          {
+            type: "text",
+            marks: [
+              {
+                type: "link",
+                attrs: {
+                  href: "https://sdk.vercel.ai/docs",
+                  target: "_blank",
+                  class:
+                    "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+                },
+              },
+            ],
+            text: "Vercel AI SDK",
+          },
+          { type: "text", text: " → " },
+          { type: "text", marks: [{ type: "code" }], text: "npm i ai" },
         ],
       },
       {
