96156c8e2a95d0b864beba9d30e73c5f5cecd9d5
Steven Tey <stevensteel97@gmail.com>
2023-06-18 18:34:09 -0500

    made highlight work for ++ as well

---
+++ ui/editor/default-content.tsx
@@ -0,0 +1,162 @@
+const DEFAULT_EDITOR_CONTENT = {
+  type: "doc",
+  content: [
+    {
+      type: "heading",
+      attrs: { level: 2 },
+      content: [{ type: "text", text: "Introducing Novel" }],
+    },
+    {
+      type: "paragraph",
+      content: [
+        {
+          type: "text",
+          text: "Novel is a Notion-style WYSIWYG editor with AI-powered autocompletions. Built with ",
+        },
+        {
+          type: "text",
+          marks: [
+            {
+              type: "link",
+              attrs: {
+                href: "https://tiptap.dev/",
+                target: "_blank",
+                class:
+                  "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+              },
+            },
+          ],
+          text: "Tiptap",
+        },
+        { type: "text", text: ", " },
+        {
+          type: "text",
+          marks: [
+            {
+              type: "link",
+              attrs: {
+                href: "https://openai.com/",
+                target: "_blank",
+                class:
+                  "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+              },
+            },
+          ],
+          text: "OpenAI",
+        },
+        { type: "text", text: ", and the " },
+        {
+          type: "text",
+          marks: [
+            {
+              type: "link",
+              attrs: {
+                href: "https://sdk.vercel.ai/docs",
+                target: "_blank",
+                class:
+                  "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+              },
+            },
+          ],
+          text: "Vercel AI SDK",
+        },
+        { type: "text", text: " → " },
+        { type: "text", marks: [{ type: "code" }], text: "npm i ai" },
+      ],
+    },
+    {
+      type: "heading",
+      attrs: { level: 3 },
+      content: [{ type: "text", text: "Learn more" }],
+    },
+    {
+      type: "orderedList",
+      attrs: { start: 1 },
+      content: [
+        {
+          type: "listItem",
+          content: [
+            {
+              type: "paragraph",
+              content: [
+                { type: "text", text: "Check out the " },
+                {
+                  type: "text",
+                  marks: [
+                    {
+                      type: "link",
+                      attrs: {
+                        href: "https://twitter.com/steventey/status/1669762868416512000",
+                        target: "_blank",
+                        class:
+                          "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+                      },
+                    },
+                  ],
+                  text: "launch video",
+                },
+                { type: "text", text: "." },
+              ],
+            },
+          ],
+        },
+        {
+          type: "listItem",
+          content: [
+            {
+              type: "paragraph",
+              content: [
+                { type: "text", text: "Star us on " },
+                {
+                  type: "text",
+                  marks: [
+                    {
+                      type: "link",
+                      attrs: {
+                        href: "https://github.com/steven-tey/novel",
+                        target: "_blank",
+                        class:
+                          "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+                      },
+                    },
+                  ],
+                  text: "GitHub",
+                },
+                { type: "text", text: "." },
+              ],
+            },
+          ],
+        },
+        {
+          type: "listItem",
+          content: [
+            {
+              type: "paragraph",
+              content: [
+                {
+                  type: "text",
+                  marks: [
+                    {
+                      type: "link",
+                      attrs: {
+                        href: "https://vercel.com/templates/next.js/novel",
+                        target: "_blank",
+                        class:
+                          "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
+                      },
+                    },
+                  ],
+                  text: "Deploy",
+                },
+                { type: "text", text: " your own." },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    { type: "paragraph" },
+  ],
+};
+
+export default DEFAULT_EDITOR_CONTENT;
--- ui/editor/index.tsx
+++ ui/editor/index.tsx
@@ -9,168 +9,13 @@ import { useDebouncedCallback } from "use-debounce";
 import { useCompletion } from "ai/react";
 import { toast } from "sonner";
 import va from "@vercel/analytics";
+import DEFAULT_EDITOR_CONTENT from "./default-content";
 
 export default function Editor() {
-  const [content, setContent] = useLocalStorage("content", {
-    type: "doc",
-    content: [
-      {
-        type: "heading",
-        attrs: { level: 2 },
-        content: [{ type: "text", text: "Introducing Novel" }],
-      },
-      {
-        type: "paragraph",
-        content: [
-          {
-            type: "text",
-            text: "Novel is a Notion-style WYSIWYG editor with AI-powered autocompletions. Built with ",
-          },
-          {
-            type: "text",
-            marks: [
-              {
-                type: "link",
-                attrs: {
-                  href: "https://tiptap.dev/",
-                  target: "_blank",
-                  class:
-                    "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
-                },
-              },
-            ],
-            text: "Tiptap",
-          },
-          { type: "text", text: ", " },
-          {
-            type: "text",
-            marks: [
-              {
-                type: "link",
-                attrs: {
-                  href: "https://openai.com/",
-                  target: "_blank",
-                  class:
-                    "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
-                },
-              },
-            ],
-            text: "OpenAI",
-          },
-          { type: "text", text: ", and the " },
-          {
-            type: "text",
-            marks: [
-              {
-                type: "link",
-                attrs: {
-                  href: "https://sdk.vercel.ai/docs",
-                  target: "_blank",
-                  class:
-                    "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
-                },
-              },
-            ],
-            text: "Vercel AI SDK",
-          },
-          { type: "text", text: " → " },
-          { type: "text", marks: [{ type: "code" }], text: "npm i ai" },
-        ],
-      },
-      {
-        type: "heading",
-        attrs: { level: 3 },
-        content: [{ type: "text", text: "Learn more" }],
-      },
-      {
-        type: "orderedList",
-        attrs: { start: 1 },
-        content: [
-          {
-            type: "listItem",
-            content: [
-              {
-                type: "paragraph",
-                content: [
-                  { type: "text", text: "Check out the " },
-                  {
-                    type: "text",
-                    marks: [
-                      {
-                        type: "link",
-                        attrs: {
-                          href: "https://twitter.com/steventey/status/1669762868416512000",
-                          target: "_blank",
-                          class:
-                            "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
-                        },
-                      },
-                    ],
-                    text: "launch video",
-                  },
-                  { type: "text", text: "." },
-                ],
-              },
-            ],
-          },
-          {
-            type: "listItem",
-            content: [
-              {
-                type: "paragraph",
-                content: [
-                  { type: "text", text: "Star us on " },
-                  {
-                    type: "text",
-                    marks: [
-                      {
-                        type: "link",
-                        attrs: {
-                          href: "https://github.com/steven-tey/novel",
-                          target: "_blank",
-                          class:
-                            "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
-                        },
-                      },
-                    ],
-                    text: "GitHub",
-                  },
-                  { type: "text", text: "." },
-                ],
-              },
-            ],
-          },
-          {
-            type: "listItem",
-            content: [
-              {
-                type: "paragraph",
-                content: [
-                  {
-                    type: "text",
-                    marks: [
-                      {
-                        type: "link",
-                        attrs: {
-                          href: "https://vercel.com/templates/next.js/novel",
-                          target: "_blank",
-                          class:
-                            "text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer",
-                        },
-                      },
-                    ],
-                    text: "Deploy",
-                  },
-                  { type: "text", text: " your own." },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      { type: "paragraph" },
-    ],
-  });
+  const [content, setContent] = useLocalStorage(
+    "content",
+    DEFAULT_EDITOR_CONTENT,
+  );
   const [saveStatus, setSaveStatus] = useState("Saved");
   const [hydrated, setHydrated] = useState(false);
 
@@ -183,21 +28,6 @@ export default function Editor() {
       setSaveStatus("Saved");
     }, 500);
   }, 750);
-
-  const { complete, completion, isLoading, stop } = useCompletion({
-    id: "novel",
-    api: "/api/generate",
-    onResponse: (response) => {
-      if (response.status === 429) {
-        toast.error("You have reached your request limit for the day.");
-        va.track("Rate Limit Reached");
-        return;
-      }
-    },
-    onError: () => {
-      toast.error("Something went wrong.");
-    },
-  });
 
   const editor = useEditor({
     extensions: TiptapExtensions,
@@ -223,6 +53,40 @@ export default function Editor() {
     },
     autofocus: "end",
   });
+
+  const { complete, completion, isLoading, stop } = useCompletion({
+    id: "novel",
+    api: "/api/generate",
+    onResponse: (response) => {
+      if (response.status === 429) {
+        toast.error("You have reached your request limit for the day.");
+        va.track("Rate Limit Reached");
+        return;
+      }
+    },
+    onFinish: (_prompt, completion) => {
+      editor?.commands.setTextSelection({
+        from: editor.state.selection.from - completion.length,
+        to: editor.state.selection.from,
+      });
+    },
+    onError: () => {
+      toast.error("Something went wrong.");
+    },
+  });
+
+  const prev = useRef("");
+
+  // Insert chunks of the generated text
+  useEffect(() => {
+    const diff = completion.slice(prev.current.length);
+    prev.current = completion;
+    editor?.commands.insertContent(diff, {
+      parseOptions: {
+        preserveWhitespace: "full",
+      },
+    });
+  }, [isLoading, editor, completion]);
 
   useEffect(() => {
     // if user presses escape or cmd + z and it's loading,
@@ -254,16 +118,6 @@ export default function Editor() {
       setHydrated(true);
     }
   }, [editor, content, hydrated]);
-
-  const prev = useRef("");
-
-  // Insert chunks of the generated text
-  useEffect(() => {
-    const diff = completion.slice(prev.current.length);
-    prev.current = completion;
-    editor?.commands.insertContent(diff);
-  }, [isLoading, editor, completion]);
-
   return (
     <div
       onClick={() => {
