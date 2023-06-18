0921657d5e9acbd5c143f9b8298c029f477c3d93
Steven Tey <stevensteel97@gmail.com>
2023-06-18 18:17:52 -0500

    switch model

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -46,7 +46,7 @@ export async function POST(req: Request): Promise<Response> {
   content = content.replace(/\n/g, " ").replace(/\/$/, "").slice(0, 5000);
 
   const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-16k",
+    model: "gpt-3.5-turbo",
     messages: [
       {
         role: "system",
