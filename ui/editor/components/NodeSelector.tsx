b7113ba0fa81d65f322c93e47f9409cde9e6a9fd
Steven Tey <stevensteel97@gmail.com>
2023-06-22 08:51:41 -0500

    small fix

--- ui/editor/components/NodeSelector.tsx
+++ ui/editor/components/NodeSelector.tsx
@@ -68,7 +68,7 @@ export const NodeSelector: FC<NodeSelectorProps> = ({
     },
   ];
 
-  const activeItem = items.find((item) => item.isActive());
+  const activeItem = items.find((item) => item.isActive()) ?? items[0];
 
   return (
     <div className="relative h-full">
