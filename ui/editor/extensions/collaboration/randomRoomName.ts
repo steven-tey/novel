fb037c52ea2a12a4565a9fecbb6838928b7616bd
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-06-25 17:25:06 +0200

    Add 3 chars long base64 suffix to room names

--- ui/editor/extensions/collaboration/randomRoomName.ts
+++ ui/editor/extensions/collaboration/randomRoomName.ts
@@ -1,10 +1,32 @@
-import { uniqueNamesGenerator, adjectives, starWars, animals } from 'unique-names-generator';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  names,
+  animals,
+} from "unique-names-generator";
+import * as crypto from "crypto";
 
 export function randomRoomName() {
-  return uniqueNamesGenerator({
-    dictionaries: [adjectives, starWars, animals],
-    separator: '-',
-    length: 3,
-    style: 'lowerCase'
-  }).replace(/ /g, '')
+  return (
+    uniqueNamesGenerator({
+      dictionaries: [adjectives, colors, animals, names],
+      separator: "-",
+      length: 4,
+      style: "lowerCase",
+    }).replace(/ /g, "") +
+    "-" +
+    getSuffix()
+  );
+}
+
+const SUFFIX_LENGTH = 3;
+const URLSAFE_BASE64_ALPHABET =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+function getSuffix() {
+  let bytes = crypto.webcrypto.getRandomValues(new Uint8Array(SUFFIX_LENGTH));
+  return Array.from(bytes)
+    .map((x) => URLSAFE_BASE64_ALPHABET[x & 63])
+    .join("");
 }
