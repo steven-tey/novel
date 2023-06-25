3abc74bf12443a591ddd5049703a522b64b0d1e0
Steven Tey <stevensteel97@gmail.com>
2023-06-25 16:45:06 -0500

    resolve more merge conflicts

--- ui/editor/index.tsx
+++ ui/editor/index.tsx
@@ -124,33 +124,6 @@ export default function Editor() {
           </>
         )}
       </main>
-      <footer className="bottom-8 flex flex-row items-center text-sm">
-        <>
-          <div
-            className={
-              "before:content-[' '] flex items-center gap-1.5 before:block before:h-2 before:w-2 before:rounded-full before:bg-stone-300 data-[status='connected']:before:bg-emerald-500"
-            }
-            data-status={connectionStatus}
-          >
-            {editor && connectionStatus === "connected"
-              ? `${editor.storage.collaborationCursor.users.length} user${
-                  editor.storage.collaborationCursor.users.length === 1
-                    ? ""
-                    : "s"
-                } online`
-              : "offline"}
-          </div>
-        </>
-        <button
-          className="ml-auto rounded-lg border border-stone-100 px-2 py-1 transition-colors hover:border-stone-400"
-          onClick={user.setName}
-          style={{
-            opacity: user.name ? 1 : 0,
-          }}
-        >
-          {user.name || "&nbsp;"}
-        </button>
-      </footer>
     </div>
   );
 }
