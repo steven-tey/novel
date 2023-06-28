92d67917f2f6962f12cc26e1ab0ecc2f6528c69c
Steven Tey <stevensteel97@gmail.com>
2023-06-27 23:04:23 -0500

    edited prompt

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -52,7 +52,8 @@ export async function POST(req: Request): Promise<Response> {
         content:
           "You are an AI writing assistant that continues existing text based on context from prior text. " +
           "Give more weight/priority to the later characters than the beginning ones. " +
-          "Make sure to construct complete sentences. Use Markdown formatting when appropriate.",
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences. " + 
+          "Use Markdown formatting when appropriate.",
       },
       {
         role: "user",
