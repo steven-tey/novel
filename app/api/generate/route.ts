97b0c498ea5913448bc2f5ddfb298025f4d3415b
Steven Tey <stevensteel97@gmail.com>
2023-06-28 10:12:21 -0500

    update prompt

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -52,8 +52,9 @@ export async function POST(req: Request): Promise<Response> {
         content:
           "You are an AI writing assistant that continues existing text based on context from prior text. " +
           "Give more weight/priority to the later characters than the beginning ones. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences. " + 
-          "Use Markdown formatting when appropriate.",
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
+        // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
+        // "Use Markdown formatting when appropriate.",
       },
       {
         role: "user",
