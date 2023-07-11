8a66a2fd4f55f881ebda18032a3fac3f0d5003c0
Z4NR34L <software@zanreal.net>
2023-07-11 20:43:47 +0200

    Added footer and feedback link

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Editor from "@/ui/editor";
 import { Toolbar } from "@/components/toolbar";
+import { Footer } from "@/components/footer";
 
 export default function Page() {
   return (
     <div className="relative min-h-screen max-w-5xl mx-auto py-16">
       <Toolbar/>
       <Editor/>
+      <Footer/>
     </div>
   );
 }
