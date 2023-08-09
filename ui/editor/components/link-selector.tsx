5cbbe2799b0385bda3945ba0ed64db515d25110f
Steven Tey <stevensteel97@gmail.com>
2023-08-09 14:10:02 -0500

    moved to a utility function

--- ui/editor/components/link-selector.tsx
+++ ui/editor/components/link-selector.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn, getUrlFromString } from "@/lib/utils";
 import { Editor } from "@tiptap/core";
 import { Check, Trash } from "lucide-react";
 import { Dispatch, FC, SetStateAction, useEffect, useRef } from "react";
@@ -43,16 +43,8 @@ export const LinkSelector: FC<LinkSelectorProps> = ({
           onSubmit={(e) => {
             e.preventDefault();
             const input = e.target[0] as HTMLInputElement;
-            let link: string = input.value.trim();
-            if (
-              link &&
-              !link.startsWith("http://") &&
-              !link.startsWith("https://")
-            ) {
-              link = "http://" + link;
-            }
-            input.value &&
-              editor.chain().focus().setLink({ href: input.value }).run();
+            const url = getUrlFromString(input.value);
+            url && editor.chain().focus().setLink({ href: url }).run();
             setIsOpen(false);
           }}
           className="fixed top-full z-[99999] mt-1 flex w-60 overflow-hidden rounded border border-stone-200 bg-white p-1 shadow-xl animate-in fade-in slide-in-from-top-1"
