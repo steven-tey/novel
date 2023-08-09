2d2cef47cf52a2c90c5a05a9f1cb9c9e68e9aff6
Steven Tey <stevensteel97@gmail.com>
2023-08-09 13:30:54 -0500

    small change

--- ui/editor/components/link-selector.tsx
+++ ui/editor/components/link-selector.tsx
@@ -43,7 +43,8 @@ export const LinkSelector: FC<LinkSelectorProps> = ({
           onSubmit={(e) => {
             e.preventDefault();
             const input = e.target[0] as HTMLInputElement;
-            input.value ? editor.chain().focus().setLink({ href: input.value }).run() : "";
+            input.value &&
+              editor.chain().focus().setLink({ href: input.value }).run();
             setIsOpen(false);
           }}
           className="fixed top-full z-[99999] mt-1 flex w-60 overflow-hidden rounded border border-stone-200 bg-white p-1 shadow-xl animate-in fade-in slide-in-from-top-1"
