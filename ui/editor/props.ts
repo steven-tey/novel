b4858526920648211ab657f624b4db8286f70b5c
Aditya Vaishnav <firehudson@gmail.com>
2023-08-24 08:36:45 +0000

    Place a guard around startImageUpload

--- ui/editor/props.ts
+++ ui/editor/props.ts
@@ -45,7 +45,10 @@ export const TiptapEditorProps: EditorProps = {
         top: event.clientY,
       });
       // here we deduct 1 from the pos or else the image will create an extra node
-      startImageUpload(file, view, (coordinates?.pos as number) - 1);
+      if (coordinates) {
+        startImageUpload(file, view, coordinates.pos - 1);
+      }
+
       return true;
     }
     return false;
