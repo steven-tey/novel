7e56963c445a429dcd6f2f62acda9676a67cb388
Mo'men Sherif <momensherif.2019@gmail.com>
2023-09-02 01:39:36 +0300

    feat: drag and drop extension for tiptap editor

--- packages/core/src/ui/editor/extensions/index.tsx
+++ packages/core/src/ui/editor/extensions/index.tsx
@@ -14,6 +14,7 @@ import SlashCommand from "./slash-command";
 import { InputRule } from "@tiptap/core";
 import UploadImagesPlugin from "@/ui/editor/plugins/upload-images";
 import UpdatedImage from "./updated-image";
+import DragAndDrop from "./drag-and-drop";
 
 export const defaultExtensions = [
   StarterKit.configure({
@@ -135,4 +136,5 @@ export const defaultExtensions = [
     html: false,
     transformCopiedText: true,
   }),
+  DragAndDrop
 ];
--- packages/core/src/ui/editor/index.tsx
+++ packages/core/src/ui/editor/index.tsx
@@ -24,7 +24,7 @@ import { Editor as EditorClass } from "@tiptap/core";
 
 export default function Editor({
   completionApi = "/api/generate",
-  className = "relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg",
+  className = "relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg",
   defaultValue = defaultEditorContent,
   extensions = [],
   editorProps = {},
