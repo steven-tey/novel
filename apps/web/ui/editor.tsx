5976e2b39d1cc0eec813a263682abe54b483073e
pisacode <mckiranli@gmail.com>
2023-10-06 16:43:06 +0300

    package name changed

--- apps/web/ui/editor.tsx
+++ apps/web/ui/editor.tsx
@@ -8,25 +8,13 @@ export default function Editor() {
   const [saveStatus, setSaveStatus] = useState('Saved');
   const editorRef = useRef<EditorClass>();
 
-  const handleSave = () => {
-    editorRef.current.commands.clearContent();
-
-    editorRef.current.commands.insertHTML('<h1>Example Text</h1>');
-  };
-
   return (
     <div className="relative w-full max-w-screen-lg">
-      <button
-        className="absolute left-5 top-5 z-10 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400"
-        onClick={handleSave}
-      >
-        Save
-      </button>
       <div className="absolute right-5 top-5 z-10 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
         {saveStatus}
       </div>
       <NovelEditor
-        setEditor={(e) => {
+        setEditor={(e: any) => {
           editorRef.current = e;
         }}
         onUpdate={() => {
