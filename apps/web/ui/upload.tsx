f26d107c65266cca311dfdab099125dfe6d6ea64
vinayak athavale <vinayakathavale@users.noreply.github.com>
2023-10-16 12:13:28 +0000

    pdf read plain text

--- apps/web/ui/upload.tsx
+++ apps/web/ui/upload.tsx
@@ -1,52 +1,73 @@
-// components/PdfTextExtractor.tsx
+// pages/PdfUploader.tsx
 "use client"
-import React, { useRef, ChangeEvent } from 'react';
-import { getDocument } from 'pdfjs-dist/es5/build/pdf';
 
-const PdfTextExtractor: React.FC = () => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
+// pages/PdfUploader.tsx
 
-  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files && event.target.files[0];
-    if (file) {
-      extractTextFromPdf(file);
+import React, { useState } from 'react';
+
+const PdfUploader: React.FC = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const uploadedFile = event.target.files?.[0];
+
+    if (uploadedFile) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const typedArray = new Uint8Array(reader.result as ArrayBuffer);
+        const script = document.createElement('script');
+
+        script.src = 'https://unpkg.com/pdfjs-dist/build/pdf.js';
+        script.onload = () => {
+          const pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+          pdfjsLib.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist/build/pdf.worker.min.js`;
+
+          pdfjsLib.getDocument(typedArray).promise.then((pdf: any) => {
+            let pdfText = '';
+
+            const maxPages = pdf.numPages;
+            const getPageText = (pageNumber: number) => {
+              pdf.getPage(pageNumber).then((page: any) => {
+                page.getTextContent().then((textContent: any) => {
+                  textContent.items.forEach((item: any) => {
+                    pdfText += item.str + ' ';
+                  });
+
+                  if (pageNumber < maxPages) {
+                    getPageText(pageNumber + 1);
+                  } else {
+                    console.log(pdfText);
+                  }
+                });
+              });
+            };
+
+            getPageText(1);
+          });
+        };
+
+        document.head.appendChild(script);
+      };
+
+      reader.readAsArrayBuffer(uploadedFile);
     }
-  };
 
-  const extractTextFromPdf = async (file: File) => {
-    try {
-      const pdf = await getDocument(URL.createObjectURL(file)).promise;
-
-      let textContent = '';
-
-      for (let i = 1; i <= pdf.numPages; i++) {
-        const page = await pdf.getPage(i);
-        const text = await page.getTextContent();
-        textContent += text.items.map(item => item.str).join(' ');
-      }
-
-      console.log(textContent);  // Log the extracted text content
-    } catch (error) {
-      console.error('Error extracting text from PDF:', error);
-    }
+    setFile(uploadedFile);
   };
 
   return (
     <div>
-      <div className="upload-section">
-        <input
-          type="file"
-          ref={fileInputRef}
-          onChange={handleFileUpload}
-          style={{ display: 'none' }}
-          accept=".pdf"  // Accept only PDF files
-        />
-        <button onClick={() => fileInputRef.current && fileInputRef.current.click()}>
-          Upload PDF
-        </button>
-      </div>
+      <h1>PDF Uploader</h1>
+      <input type="file" accept=".pdf" onChange={handleFileUpload} />
+      {file && <p>Uploaded: {file.name}</p>}
     </div>
   );
 };
 
-export default PdfTextExtractor;
+export default PdfUploader;
+
+
+
+
