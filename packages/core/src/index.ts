550d9ac2afbe7dd5823c027907efba2d0a349d6b
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2023-11-14 00:36:05 -0800

    Export defaultExtensions
    I need access to the defaultExtensions for collab reasons. Can we export it? Others might find it useful as well

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,4 +2,5 @@ import "./styles/index.css";
 import "./styles/tailwind.css";
 import "./styles/prosemirror.css";
 
+export { defaultExtensions } from "./ui/editor/extensions";
 export { default as Editor } from "./ui/editor";
