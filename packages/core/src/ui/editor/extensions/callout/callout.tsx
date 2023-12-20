96fafa5d01972615f6e22d13e8f1ed2bd405a7ab
motoyannn <motoya.kon@gmail.com>
2023-12-21 01:06:30 +0900

    fix: padding

--- packages/core/src/ui/editor/extensions/callout/callout.tsx
+++ packages/core/src/ui/editor/extensions/callout/callout.tsx
@@ -9,33 +9,35 @@ const Callout = () => {
 
   return (
     <NodeViewWrapper className="callout">
-      <div className="novel-bg-stone-100 novel-w-full novel-p-5 novel-flex novel-items-start">
-        <Popover.Root open={open}>
-          <Popover.Trigger
-            asChild
-            onClick={() => {
-              setOpen(!open);
-            }}
-          >
-            <button className="novel-ml-2 novel-pr-3 novel-text-2xl">
-              {emoji}
-            </button>
-          </Popover.Trigger>
-          <Popover.Portal>
-            <Popover.Content sideOffset={5} className="novel-ml-4">
-              <EmojiPicker
-                // NOTE: in the node view, focus on the search input can't be set
-                searchDisabled
-                onEmojiClick={({ emoji }) => {
-                  setEmoji(emoji);
-                  setOpen(false);
-                }}
-              />
-            </Popover.Content>
-          </Popover.Portal>
-        </Popover.Root>
-        <div className="novel-w-full novel-border">
-          <NodeViewContent />
+      <div className="novel-py-2">
+        <div className="novel-bg-stone-100 novel-w-full novel-p-5 novel-flex novel-items-start">
+          <Popover.Root open={open}>
+            <Popover.Trigger
+              asChild
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              <button className="novel-ml-2 novel-pr-3 novel-text-2xl">
+                {emoji}
+              </button>
+            </Popover.Trigger>
+            <Popover.Portal>
+              <Popover.Content sideOffset={5} className="novel-ml-4">
+                <EmojiPicker
+                  // NOTE: in the node view, focus on the search input can't be set
+                  searchDisabled
+                  onEmojiClick={({ emoji }) => {
+                    setEmoji(emoji);
+                    setOpen(false);
+                  }}
+                />
+              </Popover.Content>
+            </Popover.Portal>
+          </Popover.Root>
+          <div className="novel-w-full novel-border">
+            <NodeViewContent />
+          </div>
         </div>
       </div>
     </NodeViewWrapper>
