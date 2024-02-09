fcda4447faa3442e0a73b5f34e744e75199ad808
andrewdoro <elfandreis@gmail.com>
2024-02-09 20:00:37 +0200

    chore: remove old core

--- packages/core/src/index.ts
+++
@@ -1,5 +0,0 @@
-import "./styles/index.css";
-import "./styles/tailwind.css";
-import "./styles/prosemirror.css";
-
-export { default as Editor } from "./ui/editor";
--- packages/core/src/ui/icons/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as LoadingCircle } from "./loading-circle";
-export { default as Magic } from "./magic";
