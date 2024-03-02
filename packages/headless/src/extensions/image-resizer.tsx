ba2fd66b02016009b91fb931f760debe45026abf
Hayden Bleasel <hello@haydenbleasel.com>
2024-03-02 13:59:32 -0500

    Remove useless imageResizer fragment

--- packages/headless/src/extensions/image-resizer.tsx
+++ packages/headless/src/extensions/image-resizer.tsx
@@ -24,55 +24,53 @@ export const ImageResizer: FC = () => {
   };
 
   return (
-    <>
-      <Moveable
-        target={document.querySelector(".ProseMirror-selectednode") as any}
-        container={null}
-        origin={false}
-        /* Resize event edges */
-        edge={false}
-        throttleDrag={0}
-        /* When resize or scale, keeps a ratio of the width, height. */
-        keepRatio={true}
-        /* resizable*/
-        /* Only one of resizable, scalable, warpable can be used. */
-        resizable={true}
-        throttleResize={0}
-        onResize={({
-          target,
-          width,
-          height,
-          // dist,
-          delta,
-        }: // direction,
-        // clientX,
-        // clientY,
-        any) => {
-          delta[0] && (target!.style.width = `${width}px`);
-          delta[1] && (target!.style.height = `${height}px`);
-        }}
-        // { target, isDrag, clientX, clientY }: any
-        onResizeEnd={() => {
-          updateMediaSize();
-        }}
-        /* scalable */
-        /* Only one of resizable, scalable, warpable can be used. */
-        scalable={true}
-        throttleScale={0}
-        /* Set the direction of resizable */
-        renderDirections={["w", "e"]}
-        onScale={({
-          target,
-          // scale,
-          // dist,
-          // delta,
-          transform,
-        }: // clientX,
-        // clientY,
-        any) => {
-          target!.style.transform = transform;
-        }}
-      />
-    </>
+    <Moveable
+      target={document.querySelector(".ProseMirror-selectednode") as any}
+      container={null}
+      origin={false}
+      /* Resize event edges */
+      edge={false}
+      throttleDrag={0}
+      /* When resize or scale, keeps a ratio of the width, height. */
+      keepRatio={true}
+      /* resizable*/
+      /* Only one of resizable, scalable, warpable can be used. */
+      resizable={true}
+      throttleResize={0}
+      onResize={({
+        target,
+        width,
+        height,
+        // dist,
+        delta,
+      }: // direction,
+      // clientX,
+      // clientY,
+      any) => {
+        delta[0] && (target!.style.width = `${width}px`);
+        delta[1] && (target!.style.height = `${height}px`);
+      }}
+      // { target, isDrag, clientX, clientY }: any
+      onResizeEnd={() => {
+        updateMediaSize();
+      }}
+      /* scalable */
+      /* Only one of resizable, scalable, warpable can be used. */
+      scalable={true}
+      throttleScale={0}
+      /* Set the direction of resizable */
+      renderDirections={["w", "e"]}
+      onScale={({
+        target,
+        // scale,
+        // dist,
+        // delta,
+        transform,
+      }: // clientX,
+      // clientY,
+      any) => {
+        target!.style.transform = transform;
+      }}
+    />
   );
 };
