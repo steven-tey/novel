6c365b969a580cf19954e39c4ef27899efe7acb5
Hayden Bleasel <hello@haydenbleasel.com>
2024-03-02 13:49:30 -0500

    Wrapping fix

--- packages/headless/src/components/editor-command-item.tsx
+++ packages/headless/src/components/editor-command-item.tsx
@@ -20,7 +20,11 @@ export const EditorCommandItem = forwardRef<
   if (!editor || !range) return null;
 
   return (
-    <CommandItem ref={ref} {...rest} onSelect={() => onCommand({ editor, range })}>
+    <CommandItem
+      ref={ref}
+      {...rest}
+      onSelect={() => onCommand({ editor, range })}
+    >
       {children}
     </CommandItem>
   );
