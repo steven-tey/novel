1c4df17f252773ca472150541468a01c18d37588
andrewdoro <elfandreis@gmail.com>
2024-05-28 23:07:50 +0300

    fix: use verbatim import

--- packages/headless/src/extensions/slash-command.tsx
+++ packages/headless/src/extensions/slash-command.tsx
@@ -1,8 +1,8 @@
-import { RefObject } from 'react';
 import { Extension } from "@tiptap/core";
 import type { Editor, Range } from "@tiptap/core";
 import { ReactRenderer } from "@tiptap/react";
 import Suggestion, { type SuggestionOptions } from "@tiptap/suggestion";
+import type { RefObject } from "react";
 import type { ReactNode } from "react";
 import tippy, { type GetReferenceClientRect, type Instance, type Props } from "tippy.js";
 import { EditorCommandOut } from "../components/editor-command";
@@ -29,7 +29,7 @@ const Command = Extension.create({
   },
 });
 
-const renderItems = (elementRef?: RefObject<Element> | null = null) => {
+const renderItems = (elementRef?: RefObject<Element> | null) => {
   let component: ReactRenderer | null = null;
   let popup: Instance<Props>[] | null = null;
 
@@ -52,7 +52,7 @@ const renderItems = (elementRef?: RefObject<Element> | null = null) => {
       // @ts-ignore
       popup = tippy("body", {
         getReferenceClientRect: props.clientRect,
-        appendTo: () => elementRef ? elementRef.current : document.body,
+        appendTo: () => (elementRef ? elementRef.current : document.body),
         content: component.element,
         showOnCreate: true,
         interactive: true,
